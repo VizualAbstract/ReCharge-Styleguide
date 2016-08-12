fde017b1b0ee5ddc184fdb9c7118489092859026
Corey Capetillo <corey.capetillo@gmail.com>
2016-08-12 14:49:33 -0700

    Add spacing beneath contextual menus

--- _elements/input-groups-contextual.php
+++ _elements/input-groups-contextual.php
@@ -1,11 +1,9 @@
 <div class="form__group">
 	<input id="radio_group_contextual_1" type="radio" name="radio_group_contextual" class="form__radio">
 	<label for="radio_group_contextual_1" class="form__radio__label">Only storeowner is able to cancel subscription</label>
-	<p class="form__help">This is help text for the form input</p>
 
 	<input id="radio_group_contextual_1" type="radio" name="radio_group_contextual" class="form__radio">
 	<label for="radio_group_contextual_1" class="form__radio__label">Only storeowner is able to cancel subscription</label>
-	<p class="form__help">This is help text for the form input</p>
 
 	<input id="radio_group_contextual_2" type="radio" name="radio_group_contextual" checked="checked" class="form__radio">
 	<label for="radio_group_contextual_2" class="form__radio__label">Customer is able to cancel subscription</label>
@@ -31,23 +29,25 @@
 <div class="form__group">
 	<input id="checkbox_group_contextual_1" type="checkbox" name="checkbox_group_contextual" class="form__checkbox">
 	<label for="checkbox_group_contextual_1" class="form__checkbox__label">Only storeowner is able to cancel subscription</label>
-	
-	<input id="checkbox_group_contextual_1" type="checkbox" name="checkbox_group_contextual" class="form__checkbox">
-	<label for="checkbox_group_contextual_1" class="form__checkbox__label">Only storeowner is able to cancel subscription</label>
-</div>
-
-<div class="form__group">
-	<input id="checkbox_group_contextual_1" type="checkbox" name="checkbox_group_contextual" class="form__checkbox">
-	<label for="checkbox_group_contextual_1" class="form__checkbox__label">Only storeowner is able to cancel subscription</label>
-	<p class="form__help">This is help text for the form input</p>
-
-	<input id="checkbox_group_contextual_1" type="checkbox" name="checkbox_group_contextual" class="form__checkbox">
-	<label for="checkbox_group_contextual_1" class="form__checkbox__label">Only storeowner is able to cancel subscription</label>
-	<p class="form__help">This is help text for the form input</p>
+	<div class="contextual__input__options">
+		<div class="form__group">
+			<label for="chargers_before_allowed_cancellation">Allow cancellation after</label>
+			<div class="input-group">
+				<select id="chargers_before_allowed_cancellation" name="chargers_before_allowed_cancellation" class="form__select">
+					<option value="1">1</option>
+					<option value="1">2</option>
+					<option value="1">3</option>
+					<option value="1">4</option>
+				</select><!-- #chargers_before_allowed_cancellation -->
+				<div class="input-group__addon">
+					charge(s)
+				</div><!-- .form__input-group__addon -->
+			</div><!-- .input-group -->
+		</div><!-- .form__group -->
+	</div><!-- .contextual__input__options -->
 
 	<input id="checkbox_group_contextual_2" type="checkbox" name="checkbox_group_contextual" checked="checked" class="form__checkbox">
 	<label for="checkbox_group_contextual_2" class="form__checkbox__label">Customer is able to cancel subscription</label>
-	<p class="form__help">This is help text for the form input</p>
 	<div class="contextual__input__options">
 		<div class="form__group">
 			<label for="chargers_before_allowed_cancellation">Allow cancellation after</label>
