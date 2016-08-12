c441c3c233d441db1276af8aad04f1f0bb9cc429
Corey Capetillo <corey.capetillo@gmail.com>
2016-08-12 14:50:32 -0700

    Abstract form__help into an include .php file

---
+++ _elements/form-help.php
@@ -0,0 +1,17 @@
+<div class="form__group">
+	<label for="form__help" class="form__label">Form Help Example</label>
+	<input type="text" id="form__help" name="form__help" placeholder="Text input" class="form__input">
+	<p class="form__help">This is help text for the form input</p>
+</div><!-- .form__group -->
+
+<div class="form__group">
+	<input type="checkbox" id="form__checkbox__help" name="form__checkbox__help" placeholder="Text input" class="form__checkbox">
+	<label for="form__checkbox__help" class="form__checkbox__label">Form Help Example</label>
+	<p class="form__help">This is help text for the form input</p>
+</div><!-- .form__group -->
+
+<div class="form__group">
+	<input type="radio" id="form__radio__help" name="form__radio__help" placeholder="Text input" class="form__radio">
+	<label for="form__radio__help" class="form__radio__label">Form Help Example</label>
+	<p class="form__help">This is help text for the form input</p>
+</div><!-- .form__group -->
