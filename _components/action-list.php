d82cf88d1e16dcdc688e2f4a65afb5bd909ecc61
Corey Capetillo <corey.capetillo@gmail.com>
2016-08-15 16:18:09 -0700

    Modify styles on swithces so they're more dynamic. Requires testing. Add a new action list example, using switches

--- _components/action-list.php
+++ _components/action-list.php
@@ -1,5 +1,5 @@
 
-<h3>Basic Example</h3>
+<h3>Basic example</h3>
 <ul class="action-list">
 <?php for ($i = 1; $i <= 5; $i++){ ?>
 	<li class="action-list__item">
@@ -13,7 +13,7 @@
 <?php } ?>
 </ul><!-- .action-list -->
 
-<h3>With Subscription Product Media</h3>
+<h3>With subscription product media</h3>
 <ul class="action-list">
 <?php for ($i = 1; $i <= 5; $i++){ ?>
 	<li class="action-list__item">
@@ -25,6 +25,28 @@
 		</div><!-- .action-list__item__content -->
 		<div class="action-list__item__action">
 			<a href="#"><span class="fa fa-plus-circle"></span></a>
+		</div><!-- .action-list__item__action -->
+	</li><!-- .action-list__item -->
+<?php } ?>
+</ul><!-- .action-list -->
+
+<h3>With switches</h3>
+<ul class="action-list">
+<?php for ($i = 1; $i <= 3; $i++){ ?>
+	<li class="action-list__item">
+		<div class="action-list__item__content">
+			<a href="#">Subscription Activation</a>
+			<p>Sent to customers when they first sign up for a subscription</p>
+		</div><!-- .action-list__item__content -->
+		<div class="action-list__item__action text--right">
+			<label class="switch">
+				<div class="switch__inner">
+					<input class="switch__input" checked type="checkbox">
+					<span class="switch__overlay"></span>
+					<span class="switch__left">Disabled</span>
+					<span class="switch__right">Enabled</span>
+				</div><!-- .switch__inner -->
+			</label><!-- .switch -->
 		</div><!-- .action-list__item__action -->
 	</li><!-- .action-list__item -->
 <?php } ?>
