ef8f88a787c8c5dc69255864a832733c90672aa7
Corey Capetillo <corey.capetillo@gmail.com>
2016-08-24 16:20:18 -0700

    Add a smaller switch, which will have no text

--- _components/switches.php
+++ _components/switches.php
@@ -24,3 +24,12 @@
 		<span class="switch__right">Large On</span>
 	</div><!-- .switch__inner -->
 </label><!-- .switch -->
+
+<label class="switch switch--small">
+	<div class="switch__inner">
+		<input class="switch__input" type="checkbox">
+		<span class="switch__overlay"></span>
+		<span class="switch__left"></span>
+		<span class="switch__right"></span>
+	</div><!-- .switch__inner -->
+</label><!-- .switch -->
