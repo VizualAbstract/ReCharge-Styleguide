1bb299905540e9199dccc642bf71611f23a39f54
Corey Capetillo <corey.capetillo@gmail.com>
2016-08-24 16:03:38 -0700

    Expand search input groups, add no results

--- _elements/input-groups-search.php
+++ _elements/input-groups-search.php
@@ -3,22 +3,44 @@
 		<div class="input-group__addon">
 			<label for="search_terms" class="form__label">Search</label>
 		</div>
-		<input type="text" id="search_terms" name="search_terms" value="" placeholder="Search terms..." class="form__input">
+		<input type="text" id="search_terms" name="search_terms" placeholder="Search terms..." value="" class="form__input">
 		<div class="input-group__addon">
 			<button id="submit_search" class="button">
-				<span class="fa fa-search"></span>
+				Search
 			</button><!-- #submit_search .button -->
 		</div>
 	</div><!-- .form__search -->
+</form><!-- .form__search -->
+
+<form class="form__search">
 	<div class="input-group input-group--full">
 		<div class="input-group__addon">
 			<label for="search_terms" class="form__label">Search</label>
 		</div>
-		<input type="text" id="search_terms" name="search_terms" value="" placeholder="Search terms..." class="form__input">
+		<input type="text" id="search_terms" name="search_terms" placeholder="Search terms..." value="" class="form__input">
 		<div class="input-group__addon">
 			<button id="submit_search" class="button">
-				<span class="fa fa-search"></span>
+				Search
 			</button><!-- #submit_search .button -->
 		</div>
 	</div><!-- .input-group -->
 </form><!-- .form__search -->
+
+<form class="form__search form__search--active">
+	<div class="input-group input-group--full">
+		<input type="text" id="search_terms" name="search_terms" placeholder="Search terms..." value="Abskwd" class="form__input">
+		<div class="input-group__addon">
+			<button id="submit_search" class="button">
+				Search
+			</button><!-- #submit_search .button -->
+		</div>
+	</div><!-- .input-group -->
+	<p class="form__help">
+		Add the class <code>.form__search--active</code> to the <code>form</code> element to highlight the input
+	</p>
+</form><!-- .form__search -->
+
+<div class="form__search__no-results">
+	No search results. Try again.
+</div><!-- .form__search__no-results -->
+
