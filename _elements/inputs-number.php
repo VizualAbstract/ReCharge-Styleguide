fcccd63964447ff0dca82080621eb0aceaa8316d
Corey Capetillo <corey.capetillo@gmail.com>
2016-08-24 16:03:15 -0700

    Add input numbers

---
+++ _elements/inputs-number.php
@@ -0,0 +1,12 @@
+<input type="number" id="" name="" min="0" max="999" value="999" class="form__number">
+
+<label>Number select</label>
+<select id="" name="" class="form__number">
+	<option value="1">1</option>
+	<option value="2">2</option>
+	<option value="3">3</option>
+	<option value="4">4</option>
+	<option value="5">5</option>
+	<option value="999" selected="selected">999</option>
+</select><!-- .input__number -->
+item
