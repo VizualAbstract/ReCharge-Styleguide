7dfd0c157225de86ea7b5d6e33e1e37345759505
Corey Capetillo <corey.capetillo@gmail.com>
2016-08-24 16:13:17 -0700

    Add an admin-only textarea for demonstration

--- _elements/inputs-textarea.php
+++ _elements/inputs-textarea.php
@@ -1,4 +1,12 @@
 <textarea class="form__input"></textarea>
+
 <textarea placeholder="Sed posuere consectetur est at lobortis" class="form__input"></textarea>
+
 <textarea class="form__input">Sed posuere consectetur est at lobortis. Donec ullamcorper nulla non metus auctor fringilla. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Curabitur blandit tempus porttitor.</textarea>
+
 <textarea class="form__input__code">Etiam porta sem malesuada magna mollis euismod. Nullam quis risus eget urna mollis ornare vel eu leo. Vestibulum id ligula porta felis euismod semper. Vestibulum id ligula porta felis euismod semper. Cras mattis consectetur purus sit amet fermentum. Maecenas sed diam eget risus varius blandit sit.</textarea>
+
+<div class="form__group admin-recharge">
+	<label>Admin-only input</label>
+	<textarea class="form__input__code">Etiam porta sem malesuada magna mollis euismod. Nullam quis risus eget urna mollis ornare vel eu leo. Vestibulum id ligula porta felis euismod semper. Vestibulum id ligula porta felis euismod semper. Cras mattis consectetur purus sit amet fermentum. Maecenas sed diam eget risus varius blandit sit.</textarea>
+</div><!-- .form__group admin-recharge -->
