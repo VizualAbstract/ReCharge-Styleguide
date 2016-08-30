28ce153d585c1379a0186bb51048bbd24a28728a
Corey Capetillo <corey.capetillo@gmail.com>
2016-08-30 16:15:55 -0700

    Support off-status in admin-recharge switch class

--- _components/switches.php
+++ _components/switches.php
@@ -33,3 +33,32 @@
 		<span class="switch__right"></span>
 	</div><!-- .switch__inner -->
 </label><!-- .switch -->
+
+<span class="admin-recharge">
+	<label class="switch">
+		<div class="switch__inner">
+			<input class="switch__input" type="checkbox">
+			<span class="switch__overlay"></span>
+			<span class="switch__left">Off</span>
+			<span class="switch__right">On</span>
+		</div><!-- .switch__inner -->
+	</label><!-- .switch -->
+</span><!-- .admin-recharge -->
+
+<label class="admin-recharge switch switch--large">
+	<div class="switch__inner">
+		<input class="switch__input" type="checkbox">
+		<span class="switch__overlay"></span>
+		<span class="switch__left">Large Off</span>
+		<span class="switch__right">Large On</span>
+	</div><!-- .switch__inner -->
+</label><!-- .switch -->
+
+<label class="admin-recharge switch switch--small">
+	<div class="switch__inner">
+		<input class="switch__input" type="checkbox">
+		<span class="switch__overlay"></span>
+		<span class="switch__left"></span>
+		<span class="switch__right"></span>
+	</div><!-- .switch__inner -->
+</label><!-- .switch -->
