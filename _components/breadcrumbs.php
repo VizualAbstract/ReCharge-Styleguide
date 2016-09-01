2c1e6e62e6f5681dd552518a57d9a2cc94353156
Corey Capetillo <corey.capetillo@gmail.com>
2016-09-01 11:22:13 -0700

    Modify admin tools, add a max-width to admin tools layout container

--- _components/breadcrumbs.php
+++ _components/breadcrumbs.php
@@ -5,6 +5,14 @@
 </ol><!-- .breadcrumb -->
 
 <ol class="breadcrumbs">
+	<li class="crumb no-link">
+		<span><span class="fa fa-wrench"></span> Section with icon</span>
+	</li>
+	<li class="crumb"><a href="#">One level in</a></li>
+	<li class="crumb">Two levels in</li>
+</ol><!-- .breadcrumb -->
+
+<ol class="breadcrumbs">
 	<li class="crumb"><a href="#"><span class="fa fa-wrench"></span> Section with icon</a></li>
 	<li class="crumb"><a href="#">One level in</a></li>
 	<li class="crumb">Two levels in</li>
