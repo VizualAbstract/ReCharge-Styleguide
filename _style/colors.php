43c27da3da8d90c3567f0842b4f85e48eaa220fa
Corey Capetillo <corey.capetillo@gmail.com>
2016-08-31 17:12:38 -0700

    Change how colors are presented in the style guide. Seperate the brand, text and link colors

--- _style/colors.php
+++ _style/colors.php
@@ -1,51 +1,80 @@
+<span class="swatch background--blue">
+	Brand <span class="hexcode">Blue<br>#2C93AC</span>
+</span>
+
+<span class="swatch background--gray-darker">
+	Text <span class="hexcode">Gray, Darker<br>#424242</span>
+</span>
+
+<span class="swatch background--aquamarine">
+	Links <span class="hexcode">Aquamarine<br>#4DD6B7</span>
+</span>
+
+<hr>
+
 <span class="swatch background--black">
 	Black <span class="hexcode">#000000</span>
 </span>
+
 <span class="swatch background--gray-darker">
 	Gray, Darker <span class="hexcode">#424242</span>
 </span>
+
 <span class="swatch background--gray-dark">
 	Gray, Dark <span class="hexcode">#676767</span>
 </span>
+
 <span class="swatch background--gray">
 	Gray <span class="hexcode">#8E8F8E</span>
 </span>
+
 <span class="swatch background--gray-light">
 	Gray, Light <span class="hexcode">#CCCCCC</span>
 </span>
+
 <span class="swatch background--gray-lighter">
 	Gray, Lighter <span class="hexcode">#EEEEEE</span>
 </span>
+
 <span class="swatch background--white">
 	White <span class="hexcode">#FFFFFF</span>
 </span>
 <span class="swatch background--cyan">
 	Cyan <span class="hexcode">#53BAD3</span>
 </span>
+
 <span class="swatch background--blue">
 	Blue <span class="hexcode">#2C93AC</span>
 </span>
+
 <span class="swatch background--green">
 	Green <span class="hexcode">#2BAC8F</span>
 </span>
+
 <span class="swatch background--aquamarine">
 	Aquamarine <span class="hexcode">#4DD6B7</span>
 </span>
+
 <span class="swatch background--mint">
 	Mint <span class="hexcode">#BDF0E4</span>
 </span>
+
 <span class="swatch background--yellow">
 	Yellow <span class="hexcode">#FFF155</span>
 </span>
+
 <span class="swatch background--orange">
 	Orange <span class="hexcode">#FB8309</span>
 </span>
+
 <span class="swatch background--red">
 	Red <span class="hexcode">#EC3D10</span>
 </span>
+
 <span class="swatch background--magenta">
 	Magenta <span class="hexcode">#C00399</span>
 </span>
+
 <span class="swatch background--pink">
 	Pink <span class="hexcode">#D06EAB</span>
 </span>
