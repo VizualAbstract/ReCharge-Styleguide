026cab3260838933846e423703a77239af09a6b9
Corey Capetillo <corey.capetillo@gmail.com>
2016-09-08 14:19:07 -0700

    Update styles for tabbed layout, stats

--- _layouts/tabs.php
+++ _layouts/tabs.php
@@ -20,7 +20,6 @@
 			<div class="layout">
 				<div class="layout__content">
 					<p>.layout__content</p>
-					<p>&nbsp;</p><br><p>&nbsp;</p><br><p>&nbsp;</p>
 				</div><!-- .layout__content -->
 				<div class="layout__sidebar">
 					<p>.layout__sidebar</p>
@@ -34,10 +33,10 @@
 			<div class="layout">
 				<div class="layout__content">
 					<p>.layout__content</p>
-					<p>&nbsp;</p><br><p>&nbsp;</p><br><p>&nbsp;</p>
 				</div><!-- .layout__content -->
-				<div class="layout__sidebar layout__sidebar--statistics">
+				<div class="layout__sidebar layout__sidebar--stats">
 					<p>.layout__sidebar</p>
+					<p>.layout__sidebar--stats</p>
 				</div><!-- .layout__sidebar-->
 			</div><!-- .layout -->
 		</div><!-- .layout__container -->
@@ -47,17 +46,15 @@
 		<div class="layout__container">
 			<div class="layout">
 				<div class="layout__content">
-					<p>.layout__content</p>
-					<p>&nbsp;</p><br><p>&nbsp;</p><br><p>&nbsp;</p>
 				</div><!-- .layout__content -->
-				<div class="layout__sidebar layout__sidebar--statistics text--center">
+				<div class="layout__sidebar layout__sidebar--stats">
 
-					<div class="stat">
+					<div class="stat stat--info">
 						<h3 class="stat__value">173</h3>
 						<div class="stat__label">
 							Active customers
 						</div><!-- .stat__label -->
-						<span class="tooltip-button" data-toggle="rc_tooltip" data-placement="left" title="<p>Active customers may include  customers that have had their  payment info declined because  they are not yet cancelled. </p>"></span>
+						<span class="tooltip-button" data-toggle="rc_tooltip" data-placement="left" title="Tooltip help text."></span>
 					</div><!-- .stat -->
 
 					<div class="stat">
@@ -65,54 +62,10 @@
 						<div class="stat__label">
 							Recurring churn
 						</div><!-- .stat__label -->
-						<span class="tooltip-button" data-toggle="rc_tooltip" data-placement="left" title="<p>Percentage of cancelled  customers, including payment  declines between their first and  2nd charge.</p><p>Percentage does not include  customers created in the last  30 days. </p>"></span>
-					</div><!-- .stat -->
-
-					<div class="stat">
-						<h3 class="stat__value">103</h3>
-						<div class="stat__label">
-							Average number of charges
-						</div><!-- .stat__label -->
-						<span class="tooltip-button" data-toggle="rc_tooltip" data-placement="left" title="<p>Number includes customers that  have signed up in the last 30 days.</p>"></span>
-					</div><!-- .stat -->
-
-					<div class="stat">
-						<h3 class="stat__value">$10,000,000</h3>
-						<div class="stat__label">
-							Average revenue per customer
-						</div><!-- .stat__label -->
-						<span class="tooltip-button" data-toggle="rc_tooltip" data-placement="left" title="<p>Revenue does not include taxes or  shipping costs.</p>"></span>
+						<span class="tooltip-button" data-toggle="rc_tooltip" data-placement="left" title="Tooltip help text."></span>
 					</div><!-- .stat -->
 				</div><!-- .layout__sidebar-->
 			</div><!-- .layout -->
 		</div><!-- .layout__container -->
 	</div><!-- .tabs__content -->
 </div><!-- .tabs -->
-
-<style>
-.new .stat {
-	max-width: 210px;
-	margin: 0 auto 30px;
-	display: block;
-	clear: both;
-}
-
-.new .stat .rc_popover {
-	text-align: left;
-}
-
-.new .stat .popover-button,
-.new .stat .tooltip-button {
-	color: #FFF;
-}
-
-.new .stat .popover-button:hover,
-.new .stat .tooltip-button:hover {
-	color: #4dd6b7;
-}
-
-.new .stat .tooltip-box,
-.new .stat .popover-box {
-	width: 290px !important;
-}
-</style>
