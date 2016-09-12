d7c9ad7ad8a31e69c7af3793e833e2f9f78886f2
Corey Capetillo <corey.capetillo@gmail.com>
2016-09-12 15:07:25 -0700

    Add a button to date range picker

--- _elements/date-range.php
+++ _elements/date-range.php
@@ -7,4 +7,7 @@
 		<label for="end_date" class="form__label">End date</label>
 		<input type="text" name="end_date" id="end_date" value="" class="form__date">
 	</div><!-- .date-range__end -->
+	<div class="date-range__submit">
+		<button class="button">Update metrics</button>
+	</div><!-- .date-range__submit -->
 </div><!-- .date-picker -->
