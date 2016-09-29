1b7b1588b660f652f56e2144411ae07b8d6b100a
Corey Capetillo <corey.capetillo@gmail.com>
2016-09-29 10:45:07 -0700

    Add a default date fallback if defined

--- _elements/date-range.php
+++ _elements/date-range.php
@@ -1,18 +1,18 @@
-<div class="date-range">
+<div class="date-range" id="date-range__charts">
 	<div class="date-range__start">
 		<label for="start_date" class="form__label">Start date</label>
-		<input type="text" name="start_date" id="start_date" placeholder="select a date" value="2016-08-01" class="form__date">
+		<input type="text" name="start_date" id="start_date" data-default="2016-08-01" value="2016-08-01" placeholder="select a date" class="form__date">
 	</div><!-- .date-range__start -->
 	<div class="date-range__end">
 		<label for="end_date" class="form__label">End date</label>
-		<input type="text" name="end_date" id="end_date" placeholder="select a date" value="2016-08-08" class="form__date">
+		<input type="text" name="end_date" id="end_date" data-default="2016-08-08" value="2016-08-08" placeholder="select a date" class="form__date">
 	</div><!-- .date-range__end -->
 	<div class="date-range__submit">
-		<button class="button">Update metrics</button>
+		<button class="button">Update charts</button>
 	</div><!-- .date-range__submit -->
 </div><!-- .date-picker -->
 
-<div class="date-range">
+<div class="date-range" id="date-range__downloads">
 	<div class="date-range__start">
 		<label for="start_date_empty" class="form__label">Start date</label>
 		<input type="text" name="start_date_empty" id="start_date_empty" placeholder="select a date" value="" class="form__date">
@@ -21,7 +21,4 @@
 		<label for="end_date_empty" class="form__label">End date</label>
 		<input type="text" name="end_date_empty" id="end_date_empty" placeholder="select a date" value="" class="form__date">
 	</div><!-- .date-range__end -->
-	<div class="date-range__submit">
-		<button class="button">Update metrics</button>
-	</div><!-- .date-range__submit -->
 </div><!-- .date-picker -->
