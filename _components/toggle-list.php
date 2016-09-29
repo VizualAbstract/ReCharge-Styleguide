32656731927e9642a0ada90b4ab1ce1dcb7ba20f
Corey Capetillo <corey.capetillo@gmail.com>
2016-09-29 15:43:56 -0700

    Create a special toggle list for discounts

--- _components/toggle-list.php
+++ _components/toggle-list.php
@@ -7,7 +7,7 @@
 		<div class="rc_toggle-list__item__action">
 			<label class="switch switch--small">
 				<div class="switch__inner">
-					<input class="switch__input" checked type="checkbox">
+					<input type="checkbox" checked class="switch__input">
 					<span class="switch__overlay"></span>
 					<span class="switch__left"></span>
 					<span class="switch__right"></span>
@@ -26,7 +26,7 @@
 		<div class="rc_toggle-list__item__action">
 			<label class="switch switch--small">
 				<div class="switch__inner">
-					<input class="switch__input" type="checkbox">
+					<input type="checkbox" class="switch__input">
 					<span class="switch__overlay"></span>
 					<span class="switch__left"></span>
 					<span class="switch__right"></span>
@@ -41,12 +41,107 @@
 		<div class="rc_toggle-list__item__action">
 			<label class="switch switch--small">
 				<div class="switch__inner">
+					<input type="checkbox" checked class="switch__input">
+					<span class="switch__overlay"></span>
+					<span class="switch__left"></span>
+					<span class="switch__right"></span>
+				</div><!-- .switch__inner -->
+			</label><!-- .switch -->
+		</div><!-- .rc_toggle-list__item__action -->
+	</li><!-- .rc_toggle-list__item -->
+</ul><!-- .rc_toggle-list -->
+
+<hr>
+
+<ul id="discount_list" class="rc_toggle-list">
+	<li class="rc_toggle-list__item">
+		<div class="rc_toggle-list__item__content">
+			DISCOUNT 1 <label class="disabled_status">Disabled</label><br>
+			<small>Sent to customers when they first sign up for a subscription</small>
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__content">
+			1/600
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__content">
+			Sep 28, 2016
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__content">
+			Sep 28, 2016
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__action">
+			<label class="switch switch--small">
+				<div class="switch__inner">
 					<input class="switch__input" checked type="checkbox">
 					<span class="switch__overlay"></span>
 					<span class="switch__left"></span>
 					<span class="switch__right"></span>
 				</div><!-- .switch__inner -->
 			</label><!-- .switch -->
+		</div><!-- .rc_toggle-list__item__action -->
+		<div class="rc_toggle-list__item__action">
+			<a href="#" data-discount_id="195688" data-discount_code="Corey Discount 1" onclick="deleteDiscount(this);" class="delete_discount" style="font-size: 20px; position: relative; top: 2px; color: #8C8C8C;">
+				<span class="fa fa-trash"></span>
+			</a>
+		</div><!-- .rc_toggle-list__item__action -->
+	</li><!-- .rc_toggle-list__item -->
+	<li class="rc_toggle-list__item disabled">
+		<div class="rc_toggle-list__item__content">
+			CARD EXPIRED <label class="disabled_status">Disabled</label><br>
+			<small>Sent to customers when they first sign up for a subscription</small>
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__content">
+			n/a
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__content">
+			Sep 28, 2016
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__content">
+			Never
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__action">
+			<label class="switch switch--small">
+				<div class="switch__inner">
+					<input class="switch__input" checked type="checkbox">
+					<span class="switch__overlay"></span>
+					<span class="switch__left"></span>
+					<span class="switch__right"></span>
+				</div><!-- .switch__inner -->
+			</label><!-- .switch -->
+		</div><!-- .rc_toggle-list__item__action -->
+		<div class="rc_toggle-list__item__action">
+			<a href="#" data-discount_id="195688" data-discount_code="Corey Discount 1" onclick="deleteDiscount(this);" class="delete_discount" style="font-size: 20px; position: relative; top: 2px; color: #8C8C8C;">
+				<span class="fa fa-trash"></span>
+			</a>
+		</div><!-- .rc_toggle-list__item__action -->
+	</li><!-- .rc_toggle-list__item -->
+	<li class="rc_toggle-list__item">
+		<div class="rc_toggle-list__item__content">
+			CC storeowner on all customer notifications <label class="disabled_status">Disabled</label><br>
+			<small>Sent to customers when they first sign up for a subscription</small>
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__content">
+			3
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__content">
+			Sep 28, 2016
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__content">
+			Sep 28, 2016
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__action">
+			<label class="switch switch--small">
+				<div class="switch__inner">
+					<input class="switch__input" checked type="checkbox">
+					<span class="switch__overlay"></span>
+					<span class="switch__left"></span>
+					<span class="switch__right"></span>
+				</div><!-- .switch__inner -->
+			</label><!-- .switch -->
+		</div><!-- .rc_toggle-list__item__action -->
+		<div class="rc_toggle-list__item__action">
+			<a href="#" data-discount_id="195688" data-discount_code="Corey Discount 1" onclick="deleteDiscount(this);" class="delete_discount" style="font-size: 20px; position: relative; top: 2px; color: #8C8C8C;">
+				<span class="fa fa-trash"></span>
+			</a>
 		</div><!-- .rc_toggle-list__item__action -->
 	</li><!-- .rc_toggle-list__item -->
 </ul><!-- .rc_toggle-list -->
