b4c9f1931ccf8492074b4867a2cb97736ebfea8a
Corey Capetillo <corey.capetillo@gmail.com>
2016-10-04 14:25:23 -0700

    Simplify toggle list by moving font sizing into more general classes'

--- _components/toggle-list.php
+++ _components/toggle-list.php
@@ -1,10 +1,174 @@
-<ul class="rc_toggle-list">
+<ul id="discount_list" class="rc_toggle-list rc_text--small">
+	<li class="rc_toggle-list__item rc_text--base">
+		<div class="rc_toggle-list__item__content">
+			<strong>Main details</strong>
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__content">
+			<strong>Detail</strong>
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__content">
+			<strong>Detail</strong>
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__content">
+			<strong>Detail</strong>
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__action">
+		</div><!-- .rc_toggle-list__item__action -->
+		<div class="rc_toggle-list__item__action">
+		</div><!-- .rc_toggle-list__item__action -->
+	</li><!-- .rc_toggle-list__item -->
 	<li class="rc_toggle-list__item">
 		<div class="rc_toggle-list__item__content">
-			<a href="#">Subscription Activation</a> <label class="disabled_status">Disabled</label>
-			<span class="rc_detail-text">
-				<p>Sent to customers when they first sign up for a subscription</p>
-			</span><!-- .rc_detail-text -->
+			<span class="rc_text--base">
+				Name of item
+			</span>
+			<span class="disabled_status">Disabled</span>
+			<p class="rc_text--description">
+				details and details and <a href="#" target="_blank">details with an external link</a>
+			</p>
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__content">
+			1/1
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__content">
+			Aug 16, 2016
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__content">
+			Aug 25, 2016
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__action">
+			<label class="switch switch--small">
+				<div class="switch__inner">
+					<input class="switch__input" checked type="checkbox">
+					<span class="switch__overlay"></span>
+					<span class="switch__left"></span>
+					<span class="switch__right"></span>
+				</div><!-- .switch__inner -->
+			</label><!-- .switch -->
+		</div><!-- .rc_toggle-list__item__action -->
+		<div class="rc_toggle-list__item__action">
+			<a href="#">
+				<span class="fa fa-trash"></span>
+			</a>
+		</div><!-- .rc_toggle-list__item__action -->
+	</li><!-- .rc_toggle-list__item -->
+	<li class="rc_toggle-list__item">
+		<div class="rc_toggle-list__item__content">
+			<span class="rc_text--base">
+				Name of item
+			</span>
+			<span class="disabled_status">Disabled</span>
+			<p class="rc_text--description">
+				details and details and details
+			</p>
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__content">
+			1/1
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__content">
+			Aug 16, 2016
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__content">
+			Never
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__action">
+			<label class="switch switch--small">
+				<div class="switch__inner">
+					<input class="switch__input" checked type="checkbox">
+					<span class="switch__overlay"></span>
+					<span class="switch__left"></span>
+					<span class="switch__right"></span>
+				</div><!-- .switch__inner -->
+			</label><!-- .switch -->
+		</div><!-- .rc_toggle-list__item__action -->
+		<div class="rc_toggle-list__item__action">
+			<a href="#">
+				<span class="fa fa-trash"></span>
+			</a>
+		</div><!-- .rc_toggle-list__item__action -->
+	</li><!-- .rc_toggle-list__item -->
+	<li class="rc_toggle-list__item disabled">
+		<div class="rc_toggle-list__item__content">
+			<span class="rc_text--base">
+				Name of item
+			</span>
+			<span class="disabled_status">Disabled</span>
+			<p class="rc_text--description">
+				details and details and details
+			</p>
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__content">
+			1/600
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__content">
+			Aug 16, 2016
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__content">
+			Never
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__action">
+			<label class="switch switch--small">
+				<div class="switch__inner">
+					<input class="switch__input" type="checkbox">
+					<span class="switch__overlay"></span>
+					<span class="switch__left"></span>
+					<span class="switch__right"></span>
+				</div><!-- .switch__inner -->
+			</label><!-- .switch -->
+		</div><!-- .rc_toggle-list__item__action -->
+		<div class="rc_toggle-list__item__action">
+			<a href="#">
+				<span class="fa fa-trash"></span>
+			</a>
+		</div><!-- .rc_toggle-list__item__action -->
+	</li><!-- .rc_toggle-list__item -->
+	<li class="rc_toggle-list__item">
+		<div class="rc_toggle-list__item__content">
+			<span class="rc_text--base">
+				Name of item
+			</span>
+			<span class="disabled_status">Disabled</span>
+			<p class="rc_text--description">
+				details and details and <a href="#" target="_blank">details with an external link</a>
+			</p>
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__content">
+			1/1
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__content">
+			Aug 16, 2016
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__content">
+			Aug 17, 2016
+		</div><!-- .rc_toggle-list__item__content -->
+		<div class="rc_toggle-list__item__action">
+			<label class="switch switch--small">
+				<div class="switch__inner">
+					<input class="switch__input" checked type="checkbox">
+					<span class="switch__overlay"></span>
+					<span class="switch__left"></span>
+					<span class="switch__right"></span>
+				</div><!-- .switch__inner -->
+			</label><!-- .switch -->
+		</div><!-- .rc_toggle-list__item__action -->
+		<div class="rc_toggle-list__item__action">
+			<a href="#">
+				<span class="fa fa-trash"></span>
+			</a>
+		</div><!-- .rc_toggle-list__item__action -->
+	</li><!-- .rc_toggle-list__item -->
+</ul><!-- .rc_toggle-list -->
+
+<ul class="rc_toggle-list rc_text--small">
+	<li class="rc_toggle-list__item">
+		<div class="rc_toggle-list__item__content">
+			<span class="rc_text--base">
+				<a href="#">Subscription Activation</a>
+			</span>
+			<span class="disabled_status">Disabled</span>
+			<p class="rc_text--description">
+				Sent to customers when they first sign up for a subscription
+			</p>
 		</div><!-- .rc_toggle-list__item__content -->
 		<div class="rc_toggle-list__item__action">
 			<label class="switch switch--small">
@@ -19,13 +183,14 @@
 	</li><!-- .rc_toggle-list__item -->
 	<li class="rc_toggle-list__item disabled">
 		<div class="rc_toggle-list__item__content">
-			<a href="#">Card Expired</a> <label class="disabled_status">Disabled</label>
-			<span class="rc_detail-text">
-				<p>
-					Sent to your customers when their credit card is about to expire.<br>
-					Emails will be sent out at 30, 15 and 3 days before card expiration.
-				</p>
-			</span><!-- .rc_detail-text -->
+			<span class="rc_text--base">
+				<a href="#">Card Expired</a>
+			</span>
+			<span class="disabled_status">Disabled</span>
+			<p class="rc_text--description">
+				Sent to your customers when their credit card is about to expire.<br>
+				Emails will be sent out at 30, 15 and 3 days before card expiration.
+			</p>
 		</div><!-- .rc_toggle-list__item__content -->
 		<div class="rc_toggle-list__item__action">
 			<label class="switch switch--small">
@@ -40,7 +205,10 @@
 	</li><!-- .rc_toggle-list__item -->
 	<li class="rc_toggle-list__item">
 		<div class="rc_toggle-list__item__content">
-			CC storeowner on all customer notifications <label class="disabled_status">Disabled</label>
+			<span class="rc_text--base">
+				CC storeowner on all customer notifications
+			</span>
+			<span class="disabled_status">Disabled</span>
 		</div><!-- .rc_toggle-list__item__content -->
 		<div class="rc_toggle-list__item__action">
 			<label class="switch switch--small">
@@ -55,15 +223,16 @@
 	</li><!-- .rc_toggle-list__item -->
 </ul><!-- .rc_toggle-list -->
 
-<hr>
-
-<ul id="discount_list" class="rc_toggle-list">
+<ul id="discount_list" class="rc_toggle-list rc_text--small">
 	<li class="rc_toggle-list__item">
 		<div class="rc_toggle-list__item__content">
-			DISCOUNT 1 <label class="disabled_status">Disabled</label>
-			<span class="rc_detail-text">
-				<p>Sent to customers when they first sign up for a subscription</p>
-			</span><!-- .rc_detail-text -->
+			<span class="rc_text--base">
+				DISCOUNT 1
+			</span>
+			<span class="disabled_status">Disabled</span>
+			<p class="rc_text--description">
+				Sent to customers when they first sign up for a subscription
+			</p>
 		</div><!-- .rc_toggle-list__item__content -->
 		<div class="rc_toggle-list__item__content">
 			1/600
@@ -92,10 +261,13 @@
 	</li><!-- .rc_toggle-list__item -->
 	<li class="rc_toggle-list__item disabled">
 		<div class="rc_toggle-list__item__content">
-			CARD EXPIRED <label class="disabled_status">Disabled</label>
-			<span class="rc_detail-text">
-				<p>Sent to customers when they first sign up for a subscription</p>
-			</span><!-- .rc_detail-text -->
+			<span class="rc_text--base">
+				CARD EXPIRED
+			</span>
+			<span class="disabled_status">Disabled</span>
+			<p class="rc_text--description">
+				Sent to customers when they first sign up for a subscription
+			</p>
 		</div><!-- .rc_toggle-list__item__content -->
 		<div class="rc_toggle-list__item__content">
 			n/a
@@ -124,10 +296,13 @@
 	</li><!-- .rc_toggle-list__item -->
 	<li class="rc_toggle-list__item">
 		<div class="rc_toggle-list__item__content">
-			CC storeowner on all customer notifications <label class="disabled_status">Disabled</label>
-			<span class="rc_detail-text">
-				<p>Sent to customers when they first sign up for a subscription</p>
+			<span class="rc_text--base">
+				CC storeowner on all customer notifications
 			</span>
+			<span class="disabled_status">Disabled</span>
+			<p class="rc_text--description">
+				Sent to customers when they first sign up for a subscription
+			</p>
 		</div><!-- .rc_toggle-list__item__content -->
 		<div class="rc_toggle-list__item__content">
 			3
