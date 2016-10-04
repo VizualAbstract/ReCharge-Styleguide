d5a73c9630a3b4f18b372ba4b45584ed6a96731e
Corey Capetillo <corey.capetillo@gmail.com>
2016-10-04 10:53:38 -0700

    Expand tabs

--- _layouts/tabs.php
+++ _layouts/tabs.php
@@ -1,4 +1,4 @@
-<div class="tabs">
+<div class="tabs tabs--cards">
 	<div class="tabs__header">
 		<ul class="tabs__list list list--inline">
 			<li class="tabs__list__item">
@@ -32,23 +32,8 @@
 		<div class="layout__container">
 			<div class="layout">
 				<div class="layout__content">
-					<p>.layout__content</p>
 				</div><!-- .layout__content -->
 				<div class="layout__sidebar layout__sidebar--stats">
-					<p>.layout__sidebar</p>
-					<p>.layout__sidebar--stats</p>
-				</div><!-- .layout__sidebar-->
-			</div><!-- .layout -->
-		</div><!-- .layout__container -->
-	</div><!-- .tabs__content -->
-	<hr>
-	<div class="tabs__content">
-		<div class="layout__container">
-			<div class="layout">
-				<div class="layout__content">
-				</div><!-- .layout__content -->
-				<div class="layout__sidebar layout__sidebar--stats">
-
 					<div class="stat stat--info">
 						<h3 class="stat__value">173</h3>
 						<div class="stat__label">
@@ -56,7 +41,6 @@
 						</div><!-- .stat__label -->
 						<span class="tooltip-button" data-toggle="rc_tooltip" data-placement="left" title="Tooltip help text."></span>
 					</div><!-- .stat -->
-
 					<div class="stat">
 						<h3 class="stat__value">16%</h3>
 						<div class="stat__label">
