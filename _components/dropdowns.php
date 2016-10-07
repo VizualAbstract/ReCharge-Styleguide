d19d4610e7c3da0cd2d742f4b2bc53ffdecbf031
Corey Capetillo <corey.capetillo@gmail.com>
2016-10-07 10:31:44 -0700

    Background color should default to white if body class has new

--- _components/dropdowns.php
+++ _components/dropdowns.php
@@ -18,3 +18,24 @@
 		<li><a href="#">Number Subscribers per Interval</a></li>
 	</ul>
 </div><!-- .button .dropdown -->
+
+<div class="dropdown dropdown--settings dropdown--right">
+	<h3 class="dropdown__trigger" data-toggle="dropdown__menu">
+		<span class="fa fa-gear"></span>
+	</h3><!-- .dropdown__trigger -->
+	<ul class="dropdown__menu list">
+		<li><a href="#">Customers</a></li>
+		<li><a href="#">Upcoming Orders</a></li>
+		<li class="active"><a href="#">Active Link 2</a></li>
+		<li><a href="#">Processed Orders</a></li>
+		<li><a href="#">Error Orders</a></li>
+		<li><a href="#">Upcoming Pre-Paid Shipments</a></li>
+		<li><a href="#">Inactive Customers</a></li>
+		<li><a href="#">Customer Actions</a></li>
+		<li><span>No Link</span></li>
+		<li></li>
+		<li><a href="#">Customers + Next Order</a></li>
+		<li><a href="#">Number Subscribers per Item</a></li>
+		<li><a href="#">Number Subscribers per Interval</a></li>
+	</ul>
+</div><!-- .button .dropdown -->
--- _components/page-title.php
+++ _components/page-title.php
@@ -11,14 +11,7 @@
 			<h1>Refer a Friend</h1>
 		</div><!-- .info-bar__title -->
 		<div class="title-bar__content">
-			<label class="toggle toggle--labeled">
-				<div class="toggle__inner">
-					<input class="toggle__input" type="checkbox">
-					<span class="toggle__overlay"></span>
-					<span class="toggle__left"></span>
-					<span class="toggle__right"></span>
-				</div><!-- .toggle__inner -->
-			</label><!-- .toggle -->
+			<button class="button button">Cancel</button>
 			<button class="button button--primary">Save</button>
 		</div><!-- .title-bar__content -->
 	</div><!-- .title-bar -->
@@ -26,3 +19,23 @@
 		<p>Refer a friend allows you to provide customers a discount that they can share with friends via Email,  FB, and Twitter to to give them incentive to signup for a subscription. A notification will be shown at  checkout with instructions to share discounts with friends.</p>
 	</div><!-- .page-title__description-->
 </div><!-- .page-title -->
+
+
+<div class="page_header">
+	<div class="page__header__title">
+		<div class="page_header__title__container">
+			<h1>Page Header</h1>
+		</div>
+	</div>
+	<div class="page_header__controls">
+		<div class="page_header__controls__container">
+			<button class="button button">Cancel</button>
+			<button class="button button--primary">Save</button>
+		</div>
+	</div>
+	<div class="page_header__description">
+		<div class="page_header__description__container">
+			<p>Refer a friend allows you to provide customers a discount that they can share with friends via Email,  FB, and Twitter to to give them incentive to signup for a subscription. A notification will be shown at  checkout with instructions to share discounts with friends.</p>
+		</div>
+	</div>
+</div>
