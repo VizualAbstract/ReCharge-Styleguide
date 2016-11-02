05d228bfdfab25b150c8de44ebc3944b84bc2ef5
Corey Capetillo <corey.capetillo@gmail.com>
2016-11-02 14:37:57 -0700

    Switch to prepros. Remove container from navbar

--- _layouts/navbar--setup.php
+++ _layouts/navbar--setup.php
@@ -1,82 +1,80 @@
 <div class="navbar navbar--setup">
-	<div class="navbar__container">
-		<a href="#" class="navbar__brand">
-			ReCharge
-		</a><!-- .navbar__brand -->
-		<div class="navbar__toggle collapsed" data-toggle="collapse__menu" data-target="#recharge-navbar_A">
-			<i class="fa fa-bars"></i>
-		</div><!-- .navbar__toggle .collapsed -->
-		<div class="visible-xs">
-			<label class="switch">
-				<div class="switch__inner">
-					<input class="switch__input" type="checkbox" checked="checked">
-					<span class="switch__overlay"></span>
-					<span class="switch__left">Test</span>
-					<span class="switch__right">Live</span>
-				</div><!-- .switch__inner -->
-			</label><!-- .switch -->
-		</div>
-		<div id="recharge-navbar_A" class="navbar__collapse collapse">
-			<ul class="navbar__links list list--inline">
-				<li class="active">
-					<a href="#">ORDERS</a>
-				</li><!-- .active -->
-				<li>
-					<a href="#">CUSTOMERS</a>
-				</li>
-				<li>
-					<a href="#">SUBSCRIPTIONS</a>
-				</li>
-			</ul><!-- .navbar__links .list -->
-			<ul class="navbar__links navbar__links--right list list--inline">
-				<li class="hidden-xs">
-					<label class="switch">
-						<div class="switch__inner">
-							<input class="switch__input" type="checkbox" checked="checked">
-							<span class="switch__overlay"></span>
-							<span class="switch__left">Test</span>
-							<span class="switch__right">Live</span>
-						</div><!-- .switch__inner -->
-					</label><!-- .switch -->
-				</li><!-- .hidden-xs -->
-				<li class="dropdown dropdown--right">
-					<a href="#" class="dropdown__trigger" data-toggle="dropdown__menu">
-						<i class="fa fa-wrench"></i>
-						<span class="visible-xs">STORE</span>
-					</a><!-- .dropdown__trigger -->
-					<ul class="dropdown__menu list">
-						<li><a href="www.google.com" target="_blank">Subscription prices</a></li>
-						<li><a href="#">Discounts</a></li>
-						<li class="active"><a href="#">Taxes</a></li>
-						<li><a href="#">Shipping</a></li>
-						<li></li>
-						<li><a href="#">Enable/Disable ReCharge</a></li>
-						<li><a href="#">Payment processor</a></li>
-						<li><a href="#">Customer portal</a></li>
-						<li><a href="#">Checkout</a></li>
-						<li><a href="#">Site editor</a></li>
-						<li><a href="#">Notifications</a></li>
-						<li><a href="#">Translation</a></li>
-					</ul><!-- .dropdown__menu .list -->
-				</li><!-- .dropdown -->
-				<li class="dropdown dropdown--right">
-					<a href="#" class="dropdown__trigger" data-toggle="dropdown__menu">
-						<i class="fa fa-user"></i>
-						<span class="visible-xs">ACCOUNT SETTINGS</span>
-					</a><!-- .dropdown__trigger -->
-					<ul class="dropdown__menu dropdow-menu list">
-						<li><a href="#">Support center</a></li>
-						<li><a href="#">My account</a></li>
-					</ul><!-- .dropdown__menu .list -->
-				</li><!-- .dropdown -->
-				<li>
-					<a href="#" class="navbar__return">
-						POST LAUNCH
-					</a><!-- .navbar__return -->
-				</li>
-			</ul><!-- .navbar__links .list -->
-		</div><!-- .navbar__collapse .collapse -->
-	</div><!-- .navbar__container -->
+	<a href="#" class="navbar__brand">
+		ReCharge
+	</a><!-- .navbar__brand -->
+	<div class="navbar__toggle collapsed" data-toggle="collapse__menu" data-target="#recharge-navbar_A">
+		<i class="fa fa-bars"></i>
+	</div><!-- .navbar__toggle .collapsed -->
+	<div class="visible-xs">
+		<label class="switch">
+			<div class="switch__inner">
+				<input class="switch__input" type="checkbox" checked="checked">
+				<span class="switch__overlay"></span>
+				<span class="switch__left">Test</span>
+				<span class="switch__right">Live</span>
+			</div><!-- .switch__inner -->
+		</label><!-- .switch -->
+	</div>
+	<div id="recharge-navbar_A" class="navbar__collapse collapse">
+		<ul class="navbar__links list list--inline">
+			<li class="active">
+				<a href="#">ORDERS</a>
+			</li><!-- .active -->
+			<li>
+				<a href="#">CUSTOMERS</a>
+			</li>
+			<li>
+				<a href="#">SUBSCRIPTIONS</a>
+			</li>
+		</ul><!-- .navbar__links .list -->
+		<ul class="navbar__links navbar__links--right list list--inline">
+			<li class="hidden-xs">
+				<label class="switch">
+					<div class="switch__inner">
+						<input class="switch__input" type="checkbox" checked="checked">
+						<span class="switch__overlay"></span>
+						<span class="switch__left">Test</span>
+						<span class="switch__right">Live</span>
+					</div><!-- .switch__inner -->
+				</label><!-- .switch -->
+			</li><!-- .hidden-xs -->
+			<li class="dropdown dropdown--right">
+				<a href="#" class="dropdown__trigger" data-toggle="dropdown__menu">
+					<i class="fa fa-wrench"></i>
+					<span class="visible-xs">STORE</span>
+				</a><!-- .dropdown__trigger -->
+				<ul class="dropdown__menu list">
+					<li><a href="www.google.com" target="_blank">Subscription prices</a></li>
+					<li><a href="#">Discounts</a></li>
+					<li class="active"><a href="#">Taxes</a></li>
+					<li><a href="#">Shipping</a></li>
+					<li></li>
+					<li><a href="#">Enable/Disable ReCharge</a></li>
+					<li><a href="#">Payment processor</a></li>
+					<li><a href="#">Customer portal</a></li>
+					<li><a href="#">Checkout</a></li>
+					<li><a href="#">Site editor</a></li>
+					<li><a href="#">Notifications</a></li>
+					<li><a href="#">Translation</a></li>
+				</ul><!-- .dropdown__menu .list -->
+			</li><!-- .dropdown -->
+			<li class="dropdown dropdown--right">
+				<a href="#" class="dropdown__trigger" data-toggle="dropdown__menu">
+					<i class="fa fa-user"></i>
+					<span class="visible-xs">ACCOUNT SETTINGS</span>
+				</a><!-- .dropdown__trigger -->
+				<ul class="dropdown__menu dropdow-menu list">
+					<li><a href="#">Support center</a></li>
+					<li><a href="#">My account</a></li>
+				</ul><!-- .dropdown__menu .list -->
+			</li><!-- .dropdown -->
+			<li>
+				<a href="#" class="navbar__return">
+					POST LAUNCH
+				</a><!-- .navbar__return -->
+			</li>
+		</ul><!-- .navbar__links .list -->
+	</div><!-- .navbar__collapse .collapse -->
 </div><!-- .navbar -->
 
 <div class="navbar navbar--setup">
