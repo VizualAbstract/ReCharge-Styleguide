23070c6ec179e605a8d3de72a4b4f77293080ecd
Corey Capetillo <corey.capetillo@gmail.com>
2016-11-16 16:48:20 -0800

    Prepend pagination with rc_

--- _components/pagination.php
+++ _components/pagination.php
@@ -1,14 +1,14 @@
-<ul class="pagination">
-	<li class="pagination__prev">
+<ul class="rc_pagination">
+	<li class="rc_pagination__prev">
 		<a href="#">Prev</a>
-	</li>
+	</li><!-- .rc_pagination__prev -->
 	<li><a href="#">1</a></li>
 	<li><a href="#">2</a></li>
 	<li class="active"><a href="#">3</a></li>
 	<li><a href="#">4</a></li>
 	<li><a href="#">5</a></li>
 	<li><a href="#">6</a></li>
-	<li class="pagination__next">
+	<li class="rc_pagination__next">
 		<a href="#">Next</a>
-	</li>
+	</li><!-- .rc_pagination__next -->
 </ul><!-- .pagination -->
