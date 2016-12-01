8b63cd254734745c465bac0f74495e59a6481c7b
Corey Capetillo <Corey.capetillo@gmail.com>
2016-11-30 16:45:33 -0800

    Update logos

---
+++ _style/branding.php
@@ -0,0 +1,11 @@
+<h3>Logo</h3>
+<p>
+	<img src="assets/images/logo.svg" width="500" style="max-width: 500px; width: 100%;" alt="ReCharge logo"><br>
+	<img src="assets/images/logo__grey.svg" width="500" style="max-width: 500px; width: 100%;" alt="ReCharge logo"><br>
+	<img src="assets/images/logo__white.svg" width="500" style="max-width: 500px; width: 100%;" alt="ReCharge logo"><br>
+</p>
+
+<h3>Powered by</h3>
+<p>
+	<img src="assets/images/rc_widget__banner.svg" width="500" style="max-width: 500px; width: 100%;" alt="ReCharge logo">
+</p>
