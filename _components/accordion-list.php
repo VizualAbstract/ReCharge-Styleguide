d85d2e891f0a19f1780b9934641e14e9d453af46
Corey Capetillo <corey.capetillo@gmail.com>
2017-02-03 15:43:10 -0800

    Add support for inactive accordion lists and an admin version

--- _components/accordion-list.php
+++ _components/accordion-list.php
@@ -1,11 +1,11 @@
 <ul class="accordion" id="integrations">
 <?php for ($i = 1; $i <= 3; $i++) { ?>
 	<li class="accordion__header">
-		<div class="accordion__media" data-target="#google_analytics__<?php echo $i; ?>" data-toggle="collapse__menu" data-parent="#integrations">
+		<div class="accordion__media" data-toggle="collapse__menu" data-parent="#integrations" data-target="#google_analytics__<?php echo $i; ?>">
 			<img src="assets/images/thumbnail__on.png" alt="Google Analytics logo">
-		</div>
+		</div><!-- .accordion__media -->
 		<div class="accordion__description">
-			<h5 data-target="#google_analytics__<?php echo $i; ?>" data-toggle="collapse__menu" data-parent="#integrations">
+			<h5 data-toggle="collapse__menu" data-parent="#integrations" data-target="#google_analytics__<?php echo $i; ?>">
 				Google Analytics
 				<small class="pull--right">Connected</small>
 			</h5>
@@ -13,8 +13,8 @@
 				Get the data you need for marketing and business decisions.
 			</p>
 			<!-- <a href="/new/settings/integrations/google-analytics" class="admin-recharge">Edit</a> -->
-		</div>
-	</li>
+		</div><!-- .accordion__description -->
+	</li><!-- .accordion__header -->
 	<li class="accordion__details collapse" id="google_analytics__<?php echo $i; ?>">
 		<form id="integrations__google_analytics">
 			<div class="form__group">
@@ -32,6 +32,6 @@
 			<a href="http://support.rechargepayments.com/article/94-google-analytics" target="_blank" class="button button--link button--primary">Help</a>
 			<button class="button button--link button--primary pull-right">Deactivate</button>
 		</form><!-- #integrations__google_analytics -->
-	</li><!-- . -->
+	</li><!-- .accordion__details .collapse -->
 <?php } ?>
 </ul><!-- .action-list -->
